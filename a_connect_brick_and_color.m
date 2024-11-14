3dbf445e98d1411c99cb317246a004ed0b223570
TateASU <jpgaffne@asu.edu>
2024-11-13 17:09:47 -0700

    Add files via upload

--- a_connect_brick_and_color.m
+++ a_connect_brick_and_color.m
@@ -1,12 +1,32 @@
-brick = ConnectBrick('Tate');
-brick.SetColorMode(3, 2);
+%brick = ConnectBrick('Tate');
 color = brick.ColorCode(3);
 
-%yellow, green, blue
 
 if color == 5%red
     brick.MoveMotor('AD', 0);
-    pause(3);
+    pause(1);
 end
 
+if color == 2%blue
+    brick.MoveMotor('AD', 0);
+    pause(1);
+    brick.beep();
+    brick.beep();
+end
 
+if color == 3%green
+    brick.MoveMotor('AD', 0);
+    pause(1);
+    brick.beep();
+    brick.beep();
+    brick.beep();
+end
+
+if color == 4%yellow
+    brick.MoveMotor('AD', 0);
+    pause(1);
+    brick.beep();
+    brick.beep();
+    brick.beep();
+    brick.beep();
+end
--- a_forward_until_touch.m
+++ a_forward_until_touch.m
@@ -1,28 +1,65 @@
 
+firstInBlue = false;
+firstInGreen = false;
+firstInYellow = false;
+
 while true
     touch = brick.TouchPressed(4); % Read a touch sensor connected to port 4
     color = brick.ColorCode(3);
     %Port A is right
     %Port D is left
-    
+
     brick.MoveMotor('AD', 100);
     if color == 5%red
         brick.MoveMotor('AD', 0);
-        pause(3);
+        pause(1);
+    end
+    if color == 2 && firstInBlue == false%blue
+        firstInBlue = true;
+        brick.MoveMotor('AD', 0);
+        pause(1);
+        brick.beep();
+        pause(.5);
+        brick.beep();
+        pause(15);
+    end
+    if color == 3 && firstInGreen == false%green
+        firstInGreen = true;
+        brick.MoveMotor('AD', 0);
+        pause(1);
+        brick.beep();
+        pause(.5);
+        brick.beep();
+        pause(.5);
+        brick.beep();
+        pause(15);
+    end
+    if color == 4 && firstInYellow == false%yellow
+        firstInYellow = true;
+        brick.MoveMotor('AD', 0);
+        pause(1);
+        brick.beep();
+        pause(.5);
+        brick.beep();
+        pause(.5);
+        brick.beep();
+        pause(.5);
+        brick.beep();
+        pause(15);
     end
     disp(touch);
-    if touch == 1
+    if touch == 1 
         brick.MoveMotor('AD', 0);
         pause(1);
         brick.MoveMotor('AD',-80);
         pause(0.8);
         brick.MoveMotor('AD', 0);
         pause(1);
-        brick.MoveMotor('D',90);
-        brick.MoveMotor('A',-70);
+        brick.MoveMotor('D',100);
+        brick.MoveMotor('A',-40);
         pause(4);
         brick.MoveMotor('AD',0);
-        pause(1); 
+        pause(1);
     end
-    
 end
+
