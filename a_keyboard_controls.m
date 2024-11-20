aca46dd04e89eee8b07d12100a601e77c50ee121
TateASU <jpgaffne@asu.edu>
2024-11-20 16:56:57 -0700

    Add files via upload

--- a_keyboard_controls.m
+++ a_keyboard_controls.m
@@ -3,34 +3,44 @@ InitKeyboard();
 while 1
     pause(0.1);
     switch key
-        case 'uparrow'
-            %disp('Up Arrow Pressed!');
-            brick.MoveMotor('AD', 100);
+        case 'w'
+            %disp('W Pressed!');
+            brick.MoveMotor('AD', 50);
             pause(0.2);
-            brick.MoveMotor('AD', 0);
-        case 'downarrow'
-            %disp('Down Arrow Pressed!');
-            brick.MoveMotor('AD', -100);
+            brick.MoveMotor('AD',0);
+        case 's'
+            %disp('S Pressed!');
+            brick.MoveMotor('AD', -50);
             pause(0.2);
-            brick.MoveMotor('AD', 0);
-        case 'leftarrow'
-            %disp('Left Arrow Pressed!');
+            brick.MoveMotor('AD',0);
+        case 'a'
+            %disp('A Pressed!');
             brick.MoveMotor('D',-100);
             pause(0.2);
             brick.MoveMotor('A',100);
             pause(0.2);
             brick.MoveMotor('AD',0);
-        case 'rightarrow'
-            %disp('RIght Arrow Pressed!');
+        case 'd'
+            %disp('D Pressed!');
             brick.MoveMotor('D',100);
             pause(0.2);
             brick.MoveMotor('A',-100);
             pause(0.2);
             brick.MoveMotor('AD',0);
+        case 'e'
+            disp('E Pressed!');
+            brick.MoveMotor('C',-100);
+            pause(1);
+        case 'r'
+            disp('R Pressed!');
+            brick.MoveMotor('C',10);
+            pause(1);
         case 0
-            disp('No Key Pressed!');
+            disp('No Key Pressetd!');
         case 'q'
             break;
+        case 't'
+            brick.MoveMotor('AD',0);
     end
 end
 CloseKeyboard();
